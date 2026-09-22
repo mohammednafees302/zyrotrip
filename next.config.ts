ff3794a7760a979f6eaff2948c6fe5da18d609a4
Mohammed Nafees <your-real-email@gmail.com>
2026-09-22 19:12:47 +0530

    fix: allow flickr images in next config

--- next.config.ts
+++ next.config.ts
@@ -26,6 +26,10 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: "https",
+        hostname: "**.staticflickr.com",
+      },
+      {
+        protocol: "https",
         hostname: "**.supabase.co",
       },
       {
